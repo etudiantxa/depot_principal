f1cef3422a409e78bddf2cd4fd44022bc3e98b97
etudiantxa <mormangane@gmail.com>
2024-12-26 10:54:11 -0500

    message

--- TodoList/todo_list.js
+++ TodoList/todo_list.js
@@ -2,6 +2,7 @@ const taskInput = document.getElementById("taskInput");
 const addTaskBtn = document.getElementById("addTaskBtn");
 const taskList = document.getElementById("taskList");
 const clearCompletedBtn = document.getElementById("clearCompletedBtn");
+const clearAllBtn = document.getElementById("clearAllBtn"); // Sélectionne le bouton
 let tasks = [];
 function addTask() {
             const taskText = taskInput.value.trim();
@@ -29,5 +30,11 @@ function clearCompletedTasks() {
             tasks = tasks.filter(task => !task.completed);
             displayTasks();
         }
+function clearAllTasks() {
+    tasks = []; // Vide le tableau tasks
+    displayTasks(); // Met à jour l'affichage
+}
+
 addTaskBtn.addEventListener("click", addTask);
-clearCompletedBtn.addEventListener("click", clearCompletedTasks);   
+clearCompletedBtn.addEventListener("click", clearCompletedTasks);
+clearAllBtn.addEventListener("click", clearAllTasks); // Ajoute l'écouteur d'événement
