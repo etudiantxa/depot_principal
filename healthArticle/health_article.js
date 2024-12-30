845b8a82aebc9a7ad730b8d594ad580786596ce6
etudiantxa <mormangane@egmail.com>
2024-12-30 13:53:31 -0500

    message

--- healthArticle/health_article.js
+++ healthArticle/health_article.js
@@ -1,12 +1,50 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>Articles de Santé</title>
-</head>
-<body>
-  <h1>Articles de Santé</h1>
-  <div id="articles"></div>
-<script src="./health_article.js"></script>
-</body>
-</html>
+var xhr = new XMLHttpRequest();
+var url = './health_article.json';
+xhr.open('GET', url, true);
+xhr.responseType = 'json';
+xhr.onload = function() {
+    var articles = xhr.response.articles;
+    var articlesDiv = document.getElementById('articles');
+    articles.forEach(function(article) {
+        var articleDiv = document.createElement('div');
+        articleDiv.classList.add('article');
+  
+        var title = document.createElement('h2');
+        title.textContent = article.title;
+  
+        var description = document.createElement('p');
+        description.textContent = article.description;
+  
+        var waysHeader = document.createElement('h3');
+        waysHeader.textContent = 'Moyens d\'atteindre :';
+  
+        var waysList = document.createElement('ul');
+        article.ways_to_achieve.forEach(function(way) {
+          var listItem = document.createElement('li');
+          listItem.textContent = way;
+          waysList.appendChild(listItem);
+        });
+  
+        var benefitsHeader = document.createElement('h3');
+        benefitsHeader.textContent = 'Avantages :';
+  
+        var benefitsList = document.createElement('ul');
+        article.benefits.forEach(function(benefit) {
+          var listItem = document.createElement('li');
+          listItem.textContent = benefit;
+          benefitsList.appendChild(listItem);
+        });
+  
+        articleDiv.appendChild(title);
+        articleDiv.appendChild(description);
+        articleDiv.appendChild(waysHeader);
+        articleDiv.appendChild(waysList);
+        articleDiv.appendChild(benefitsHeader);
+        articleDiv.appendChild(benefitsList);
+  
+        articlesDiv.appendChild(articleDiv);
+      });
+}
+xhr.send();
+
+
